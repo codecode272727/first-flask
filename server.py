f5976bb2a2721e084a40d3a0329bb21c5010ec0c
Hayden <Codecode272727@gmail.com>
2019-04-30 20:11:03 +0800

    'user/username'

--- server.py
+++ server.py
@@ -2,10 +2,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")  # Revisit decorators if you unclear of this syntax
+@app.route("/")
 def index():
     return '<h1>Why so easy</h1>'
 
 
-if __name__ == '__main__':  # Revisit previous challenge if you're uncertain what this does https://school.nextacademy.com/courses/full-stack-web-development-bootcamp-with-python/lessons/2947
+@app.route('/user/<username>')
+def show(username):
+    return f"Hi {username[0:3]}"
+
+
+if __name__ == '__main__':
     app.run()
